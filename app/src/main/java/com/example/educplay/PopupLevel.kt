a634f5ded18845a0675b7f32a1165976c43e38b7
Genina Tano <tanogenina@gmail.com>
2025-04-15 21:31:07 +0800

    popup.kt fullscreenactivity

--- app/src/main/java/com/example/educplay/PopupLevel.kt
+++ app/src/main/java/com/example/educplay/PopupLevel.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class PopupLevel : AppCompatActivity() {
+class PopupLevel : FullscreenActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
